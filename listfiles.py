6317729073f8b5c6004e3d56f8f2581d0dcf60a2
saiteja <boyapatisaiteja@gmail.com>
2025-03-06 13:40:13 +0530

    testing

--- listfiles.py
+++ listfiles.py
@@ -1,21 +1,21 @@
-import sys
+import os
 
 def process_file(file_path):
     print(f"Processing file: {file_path}")
 
 if __name__ == "__main__":
-    files = sys.argv[1:]  # Get files passed from GitHub Actions
+    files = os.getenv("files")  # Get files passed from GitHub Actions
 
     if not files:
         print("No modified files provided.")
-        sys.exit(0)
+        os.exit(0)
 
     # Filter files that are inside the 'config/' directory
     config_files = [file for file in files if file.startswith("config/")]
 
     if not config_files:
         print("No modified files in the 'config/' folder.")
-        sys.exit(0)
+        os.exit(0)
 
     for file in config_files:
         process_file(file)
