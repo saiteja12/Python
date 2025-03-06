e743319079f41ce71b61d1aab56954d0ec4c419e
saiteja <boyapatisaiteja@gmail.com>
2025-03-06 13:47:51 +0530

    test

--- listfiles.py
+++ listfiles.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def process_file(file_path):
     print(f"Processing file: {file_path}")
@@ -8,14 +9,14 @@ if __name__ == "__main__":
 
     if not files:
         print("No modified files provided.")
-        os.exit(0)
+        sys.exit(0)
 
     # Filter files that are inside the 'config/' directory
     config_files = [file for file in files if file.startswith("config/")]
 
     if not config_files:
         print("No modified files in the 'config/' folder.")
-        os.exit(0)
+        sys.exit(0)
 
     for file in config_files:
         process_file(file)
